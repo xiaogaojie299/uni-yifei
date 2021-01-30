363ffc1f46ed5b4fc2f5529a3d6cf6ae3fab86f7
肖高杰 <8106645+xiao-gaojie@user.noreply.gitee.com>
2021-01-30 21:23:25 +0800

    测试

---
+++ .history/src/utils/plugs_20210130211139.js
@@ -0,0 +1,11 @@
+export const pageTop = (duration=0)=>{
+    uni.pageScrollTo({
+        scrollTop: 0,
+        duration: duration
+        });
+}
+export const goLogin =  ()=>{
+    // window.location.replace("http://47.108.162.242:8081/index.html#/");
+    // window.location.replace("http://192.168.0.51:8082/");
+    window.location.replace("http://m.jaylfw.com/");
+} 
