449f63bab87c2ed6986c394e496a8da2f5800ba5
肖高杰 <8106645+xiao-gaojie@user.noreply.gitee.com>
2021-01-31 20:32:58 +0800

    监控中心

--- src/pages-mw/mw/collect.vue
+++ src/pages-mw/mw/collect.vue
@@ -1,66 +1,89 @@
 <template>
   <view class="container">
-      <u-sticky>
-        <view class="filter-box">
-            <!-- 关键词搜索框 -->
-            <view class="filter-search">
-              <u-search placeholder="输入医废编号、操作人员查询" v-model="keyWord" :show-action="false" @search="reload()" @blur="reload()"></u-search>
-            </view>
-            <view class="filter-tools">
-                <mw-select :default-value="mwSelectDefaultValue" :options="options" @confirm="searchConfirm"/>
-            </view>
+    <u-sticky>
+      <view class="filter-box">
+        <!-- 关键词搜索框 -->
+        <view class="filter-search">
+          <u-search
+            placeholder="输入医废编号、操作人员查询"
+            v-model="keyWord"
+            :show-action="false"
+            @search="reload()"
+            @blur="reload()"
+          ></u-search>
         </view>
-      </u-sticky>
-      <view class="list-container">
-        <s-loading v-show="loading" />
-        <view style="height: 70rpx;line-height: 70rpx;width: 100%;text-align: center;" v-show="!loading && list.length == 0">没有更多数据了</view>
-        <trace-card v-for="(item, index) in list" :key="index" :item="item" @remove="remove(index)" :options="traceOptions" mode="collect"/>
+        <view class="filter-tools">
+          <mw-select
+            :default-value="mwSelectDefaultValue"
+            :options="options"
+            @confirm="searchConfirm"
+          />
+        </view>
       </view>
+    </u-sticky>
+    <view class="list-container">
+      <s-loading v-show="loading" />
+      <view
+        style="height: 70rpx;line-height: 70rpx;width: 100%;text-align: center;"
+        v-show="!loading && list.length == 0"
+        >没有更多数据了</view
+      >
+      <trace-card
+        v-for="(item, index) in list"
+        :key="index"
+        :item="item"
+        @remove="remove(index)"
+        :options="traceOptions"
+        mode="collect"
+      />
+    </view>
   </view>
 </template>
 <script>
-import mwSelect from '@/compontens/mw-select';
-import traceCard from '@/compontens/trace-card';
-import sLoading from '@/compontens//s-loading';
+import mwSelect from "@/compontens/mw-select";
+import traceCard from "@/compontens/trace-card";
+import sLoading from "@/compontens//s-loading";
 import { listMedicalTrace } from "@/utils/api.js";
-import mwSelectMixin from '@/mixins/mw-select.js';
-export default { 
-  mixins: [ mwSelectMixin ],
-  components:{
-    mwSelect, traceCard, sLoading
+import mwSelectMixin from "@/mixins/mw-select.js";
+export default {
+  mixins: [mwSelectMixin],
+  components: {
+    mwSelect,
+    traceCard,
+    sLoading,
   },
   data() {
     return {
-        options: {
-          cascade: true,
-          department: true,
-          status: false,
-          waste: true,
-          timestamp: true
-        },
-        traceOptions: {
-          detail: true
-        },
-        loading: false,
-        pages: 0,
-        total: 0,
-        pageNo: 1,
-        pageSize: 10,
-        auditStatus: '', // 审核状态
-        startTime: '',
-        endTime: '',
-        hospitalId: '', // 医院ID
-        departmentId: '', // 科室ID
-        status: '', // 状态
-        transitCompany: '', // 搜索关键词
-        transitConfigId: 0, // 出库配置ID
-        wasteType: '', // 医废类型
-        keyWord: '',
-        list: [],
+      options: {
+        cascade: true,
+        department: true,
+        status: false,
+        waste: true,
+        timestamp: true,
+      },
+      traceOptions: {
+        detail: true,
+      },
+      loading: false,
+      pages: 0,
+      total: 0,
+      pageNo: 1,
+      pageSize: 10,
+      auditStatus: "", // 审核状态
+      startTime: "",
+      endTime: "",
+      hospitalId: "", // 医院ID
+      departmentId: "", // 科室ID
+      status: "", // 状态
+      transitCompany: "", // 搜索关键词
+      transitConfigId: 0, // 出库配置ID
+      wasteType: "", // 医废类型
+      keyWord: "",
+      list: [],
     };
   },
   onLoad(option) {
-      // this.reload();    // 这个接口掉了两次 所以数据重复 ,我就先注释这个^_^
+    this.reload(); // 这个接口掉了两次 所以数据重复 ,我就先注释这个^_^
   },
   onPullDownRefresh() {
     this.reload();
@@ -69,70 +92,73 @@ export default {
     this.next();
   },
   methods: {
-      remove(index) {
-        this.list.splice(index, 1);
-      },
-      reload() {
-        this.pageNo = 1;
-        this.pageSize = 10;
-        this.list = [];
-        this.total = 0;
-        this.pages = 0;
-        this.paginate(); 
-      },
-      // 加载下一页
-      next() {
-        if (this.pageNo >= this.pages) {
-          uni.showToast({
-            title: '没有更多了',
-            icon: 'none'
-          });
-          return ;
-        }
+    remove(index) {
+      this.list.splice(index, 1);
+    },
+    reload() {
+      this.pageNo = 1;
+      this.pageSize = 10;
+      this.list = [];
+      this.total = 0;
+      this.pages = 0;
+      this.paginate();
+    },
+    // 加载下一页
+    next() {
+      if (this.pageNo >= this.pages) {
+        uni.showToast({
+          title: "没有更多了",
+          icon: "none",
+        });
+        return;
+      }
 
-        this.pageNo++;
-        this.paginate();
-      },
-      // 加载数据
-      async paginate() {
-        this.loading = true;
+      this.pageNo++;
+      this.paginate();
+    },
+    // 加载数据
+    async paginate() {
+      this.loading = true;
 
-        listMedicalTrace({
-          pageNo: this.pageNo,
-          pageSize: this.pageSize,
-          hospitalId: this.hospitalId,
-          departmentId: this.departmentId,
-          status: this.status,
-          wasteType: this.wasteType,
-          startTime: this.startTime,
-          endTime: this.endTime,
-          keyWord: this.keyWord
-        }).then(resp => {
-            if (resp.code == 200) {
-              this.list = [...this.list, ...resp.result.records];
-              this.total = resp.result.total;
-              this.pages = resp.result.pages;
-            }
-        }).catch(err => {}).finally(e => {
+      listMedicalTrace({
+        pageNo: this.pageNo,
+        pageSize: this.pageSize,
+        hospitalId: this.hospitalId,
+        departmentId: this.departmentId,
+        status: this.status,
+        wasteType: this.wasteType,
+        startTime: this.startTime,
+        endTime: this.endTime,
+        keyWord: this.keyWord,
+      })
+        .then((resp) => {
+          if (resp.code == 200) {
+            this.list = [...this.list, ...resp.result.records];
+            this.total = resp.result.total;
+            this.pages = resp.result.pages;
+          }
+        })
+        .catch((err) => {})
+        .finally((e) => {
           this.loading = false;
           uni.stopPullDownRefresh();
-        })
-      },
-      searchConfirm(e) {
-        // 医院ID
-        this.hospitalId = e.cascade;
-        
-        this.departmentId = e.department;
-        // 审核状态
-        this.status = e.status;
-        // 医废类型
-        this.wasteType = e.waste;
-        // 时间
-        this.startTime = e.startTime;
-        this.endTime = e.endTime;
-        this.reload();
-      }
-  }
+        });
+    },
+    searchConfirm(e) {
+      // 医院ID
+      this.hospitalId = e.cascade;
+
+      this.departmentId = e.department;
+      // 审核状态
+      this.status = e.status;
+      // 医废类型
+      this.wasteType = e.waste;
+      // 时间
+      this.startTime = e.startTime;
+      this.endTime = e.endTime;
+      this.reload();
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -140,22 +166,21 @@ page {
   overflow: hidden;
 }
 .container {
-    .filter-box {
-        background: #1539AF;
-        height: 180rpx;
-        width: 100%;
-        .filter-search {
-            margin: 0rpx 28rpx;
-            height: 80rpx;
-        }
-        .filter-tools {
-
-        }
+  .filter-box {
+    background: #1539af;
+    height: 180rpx;
+    width: 100%;
+    .filter-search {
+      margin: 0rpx 28rpx;
+      height: 80rpx;
     }
-    .list-container {
-      min-height: 100vh;
-      // height: calc(100vh - 180rpx);
-      background: #F3F5F7;
+    .filter-tools {
     }
+  }
+  .list-container {
+    min-height: 100vh;
+    // height: calc(100vh - 180rpx);
+    background: #f3f5f7;
+  }
 }
 </style>
--- src/pages/public-page/select-division.vue
+++ src/pages/public-page/select-division.vue
@@ -1,40 +1,46 @@
 <template>
-<!-- 选择科室,部门,组织 -->
-<view class="">
-        <u-sticky>
-    <view class="header">
-			<view class="header-cont my-box">
-                <!-- 顶部input -->
-                <view class="ipt-box">
-                    <!-- icon -->
-                    <img :src="require('@/static/images/search.png')" alt="" />
-                    <!-- input框 -->
-                    <input @confirm="search()" confirm-type="search" type="text" v-model="name" placeholder="请输入医院完整名称搜索" />
-                </view>
-            </view>
-        </view> 
-		</u-sticky>
-       <view class="content">
-        <scroll-view scroll-y="true" style="height:calc(100vh - 300rpx)">
-          <ly-tree
-            ref="tree"
-            :isEdit="false"
-            :treeData="treeData"
-            show-radio
-            :node-key="treeDataOpt.nodeKey"
-            :filter-node-method="treeDataOpt.filterNode"
-            expand-on-click-node
-            checkOnClickNode
-            :highlight-current="true"
-            :default-expanded-keys="expandKeys"
-            :default-checked-keys="checkedKeys"
-            @check="handleCheck"
-            @radio-change="handleRadioChange"
-            @node-click="handleNodeClick"
-            :checkOnlyLeaf="pageParams.checkOnlyLeaf"
-          ></ly-tree>
+  <!-- 选择科室,部门,组织 -->
+  <view class="">
+    <u-sticky>
+      <view class="header">
+        <view class="header-cont my-box">
+          <!-- 顶部input -->
+          <view class="ipt-box">
+            <!-- icon -->
+            <img :src="require('@/static/images/search.png')" alt="" />
+            <!-- input框 -->
+            <input
+              @confirm="search()"
+              confirm-type="search"
+              type="text"
+              v-model="name"
+              placeholder="请输入医院完整名称搜索"
+            />
+          </view>
+        </view>
+      </view>
+    </u-sticky>
+    <view class="content">
+      <scroll-view scroll-y="true" style="height:calc(100vh - 300rpx)">
+        <ly-tree
+          ref="tree"
+          :isEdit="false"
+          :treeData="treeData"
+          show-radio
+          :node-key="treeDataOpt.nodeKey"
+          :filter-node-method="treeDataOpt.filterNode"
+          expand-on-click-node
+          checkOnClickNode
+          :highlight-current="true"
+          :default-expanded-keys="expandKeys"
+          :default-checked-keys="checkedKeys"
+          @check="handleCheck"
+          @radio-change="handleRadioChange"
+          @node-click="handleNodeClick"
+          :checkOnlyLeaf="pageParams.checkOnlyLeaf"
+        ></ly-tree>
 
-          <!-- <ly-tree :tree-data="treeData"
+        <!-- <ly-tree :tree-data="treeData"
                         :isEdit="false" 
                         ref="tree" 
                         node-key="id" 
@@ -42,51 +48,56 @@
                         checkOnClickNode
                         @node-click="handleNodeClick" 
                     /> -->
-        </scroll-view>
-          <view class="btn-group">
-            <view v-if="checkoutValue.checkedKeys.length==0"  class="opcity">
-                确定
-            </view>
-            <view v-else @tap="submit">
-                确定
-            </view>
+      </scroll-view>
+      <view class="btn-group">
+        <view v-if="checkoutValue.checkedKeys.length == 0" class="opcity">
+          确定
+        </view>
+        <view v-else @tap="submit">
+          确定
         </view>
       </view>
+    </view>
     <!-- <u-button @click="test">测试</u-button> -->
-</view>
+  </view>
 </template>
 
 <script>
-    /* 选择树状图 */
-    import {listRegionChildren ,listRegion,sysDepartmentTreeList,getMyDepartmentTreeList} from '@/utils/api' 
-    import LyTree from '@/compontens/tree/ly-tree/ly-tree.vue'
-	export default {
-		components: {
-			LyTree
-        },
-        props:{
-            /* 树状图数据 默认科室 */
-            
-            isEdit:{
-                type:Boolean,
-                default:true
-            },
-        },
-        computed:{
-            treeData(){
-                let checkOnlyLeaf = this.pageParams.checkOnlyLeaf;   // 判断树结构显示的数据 为true只能选择医院
-                let hospital = this.pageParams.hospital;                // 选择医院 也可选择科室
-                if(hospital){
-                    this.treeDataOpt.nodeKey = "value";
-                    // getMyDepartmentTreeList().then(({result})=>{
-                    //     console.log("result===>",result);
-                    //     return result
-                    // })
-                     return JSON.parse(localStorage.getItem("leftTreeData"))
-                }
-                if(checkOnlyLeaf){
-                    this.treeDataOpt.nodeKey = "value";
-                    /* 
+/* 选择树状图 */
+import {
+  listRegionChildren,
+  listRegion,
+  sysDepartmentTreeList,
+  getMyDepartmentTreeList,
+} from "@/utils/api";
+import LyTree from "@/compontens/tree/ly-tree/ly-tree.vue";
+export default {
+  components: {
+    LyTree,
+  },
+  props: {
+    /* 树状图数据 默认科室 */
+
+    isEdit: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    treeData() {
+      let checkOnlyLeaf = this.pageParams.checkOnlyLeaf; // 判断树结构显示的数据 为true只能选择医院
+      let hospital = this.pageParams.hospital; // 选择医院 也可选择科室
+      if (hospital) {
+        this.treeDataOpt.nodeKey = "value";
+        // getMyDepartmentTreeList().then(({result})=>{
+        //     console.log("result===>",result);
+        //     return result
+        // })
+        return JSON.parse(localStorage.getItem("leftTreeData"));
+      }
+      if (checkOnlyLeaf) {
+        this.treeDataOpt.nodeKey = "value";
+        /* 
                         this.treeDataOpt.props = function(){
                             return {
                             label: 'label', // 指把数据中的‘personName’当做label也就是节点名称
@@ -94,208 +105,206 @@
                         }
                     }
                     */
-                    return JSON.parse(localStorage.getItem("hospital"))
-                }else{
-                    
-                    getMyDepartmentTreeList().then(({result})=>{
-                        this.treeDataOpt.nodeKey = "id";
-                        return result
-                    })
-                    // return JSON.parse(localStorage.getItem("treeData"))
-                    // return this.$store.state.leftTreeData
-                }
-            },
+        return JSON.parse(localStorage.getItem("hospital"));
+      } else {
+        getMyDepartmentTreeList().then(({ result }) => {
+          this.treeDataOpt.nodeKey = "id";
+          return result;
+        });
+        // return JSON.parse(localStorage.getItem("treeData"))
+        // return this.$store.state.leftTreeData
+      }
+    },
+  },
+  data() {
+    return {
+      name: "", //输入框搜索的医院
+      defaultData: [], // 默认显示树状图数据结构
+      props: function() {
+        return {
+          // 这里的label就可以使用函数进行自定义的渲染了
+          label(data, node) {
+            return node.data.departName;
+          },
+          //label: 'departName', // 指把数据中的‘personName’当做label也就是节点名称
+          children: "childrenList", // 指把数据中的‘childs’当做children当做子节点数据
+        };
+      },
+      isshow: 2,
+      checkoutValue: {
+        checkedKeys: [],
+      },
+      expandKeys: [], //默认展开的节点
+      checkedKeys: [], // 默认选中的节点
+      pageParams: {}, // 上个页面传过来的参数 checkOnlyLeaf 属性默认只能选择最后一层
+      treeDataOpt: {
+        nodeKey: "", // 树节点唯一的节点
+        filterNode(value, data) {
+          // 查询
+          if (!value) return true;
+          return data.label.indexOf(value) !== -1;
         },
-		data() {
-			return {
-                name:"", //输入框搜索的医院
-                defaultData:[],     // 默认显示树状图数据结构
-                props: function() {
-                    return {
-                        // 这里的label就可以使用函数进行自定义的渲染了
-                        label(data, node) {
-                            return node.data.departName;
-                        },
-                        //label: 'departName', // 指把数据中的‘personName’当做label也就是节点名称
-                        children: 'childrenList' // 指把数据中的‘childs’当做children当做子节点数据
-                    }
-                },
-                isshow:2,
-                checkoutValue:{ 
-                    checkedKeys:[]
-                },
-                expandKeys:[], //默认展开的节点  
-                checkedKeys:[],  // 默认选中的节点
-                pageParams : {}, // 上个页面传过来的参数 checkOnlyLeaf 属性默认只能选择最后一层
-                treeDataOpt:{
-                    nodeKey:"", // 树节点唯一的节点
-                    filterNode(value, data) {   // 查询
-                        if (!value) return true;
-                        return data.label.indexOf(value) !== -1;
-                    },
-                },
-            };
-		},
-		// 如果不需要不用到这些方法，需要删除相应代码，打印大量日志会造成性能损耗
-		onLoad(options) {
-            this.pageParams = JSON.parse(options.params);
+      },
+    };
+  },
+  // 如果不需要不用到这些方法，需要删除相应代码，打印大量日志会造成性能损耗
+  onLoad(options) {
+    this.pageParams = JSON.parse(options.params);
+    this.$nextTick(() => {
+      // expand-current-node-parent配置表示展开当前节点的父节点
+      //this.$refs.tree.setCurrentKey(9);
+    });
+  },
 
-			this.$nextTick(() => {
-				// expand-current-node-parent配置表示展开当前节点的父节点
-				//this.$refs.tree.setCurrentKey(9);
-			});
-        },
-        
-        onShow(){
-            this.init()
-        },
-        created(){
-           this.$store.dispatch("getLeftTreeData")
-                // sysDepartmentTreeList().then(({result})=>{
-                //     uni.setStorageSync("treeData",JSON.stringify(result))
-                // })
-                this.getTreeDalta()
-        },
-        methods:{
-            init() {
-                this.expandKeys = [];
-                this.checkedKeys= [];
-                if(JSON.stringify(this.pageParams) !== '{}'){   //判断用户之前已经选择好了的id，默认展开
-                setTimeout(()=> {
-                    this.expandKeys.push(this.pageParams.parentId);
-                    this.checkedKeys= [this.pageParams.value];
-                    console.log(this.checkedKeys);
-                }, 500);
-            }
-            },
-            handleNodeClick(){
-                console.log("你好");
-            },
-            handleNodeClick(obj) {
-            },
-            handleCheck(node) {
-            this.checkoutValue = node
-            },
-            handleRadioChange(obj) {
-            },
-           
-            search(){
-                console.log(this.$refs.tree.filter);
-                this.$refs.tree.filter(this.name)
-            },
-            goAddDepart(){
-                uni.navigateTo({
-                    url:"./add-depart"
-                })
-            },
-            getTreeDalta(data){
-                getMyDepartmentTreeList().then(({code,result})=>{
-                    console.log("result==>",result);
-                    this.defaultData = result;
-                })
-            },
-            handleNodeExpand(obj) {
-                // console.log('handleNodeExpand', JSON.stringify(obj));
-            },
-            submit(){   //点击确定
-            let checkedNodes = this.checkoutValue.node;
-            console.log("checkedNodes",checkedNodes);
-            // checkedNodes:this.checkoutValue.checkedNodes[0]
-            // let pages = getCurrentPages();
-            // var prevPage = pages[pages.length - 2];  //上一个页面
-            // prevPage._data.checkedNodes = this.checkoutValue.checkedNodes[0];
-            let params = {
-                label:checkedNodes.label,
-                value:checkedNodes.data.value,
-                parentId:checkedNodes.parentId
-            }
-            let pages = getCurrentPages();
-            var prevPage = pages[pages.length - 2];  //上一个页面
-            prevPage._data.selectTree = params; // 将选择是值传到上一个页面
-            // this.$store.commit('setCheckedNodes',params);
-            uni.navigateBack();
-            }
-        }
-	};
+  onShow() {
+    this.init();
+  },
+  created() {
+    this.$store.dispatch("getLeftTreeData");
+    // sysDepartmentTreeList().then(({result})=>{
+    //     uni.setStorageSync("treeData",JSON.stringify(result))
+    // })
+    // this.getTreeDalta()
+  },
+  methods: {
+    init() {
+      this.expandKeys = [];
+      this.checkedKeys = [];
+      if (JSON.stringify(this.pageParams) !== "{}") {
+        //判断用户之前已经选择好了的id，默认展开
+        setTimeout(() => {
+          this.expandKeys.push(this.pageParams.parentId);
+          this.checkedKeys = [this.pageParams.value];
+          console.log(this.checkedKeys);
+        }, 500);
+      }
+    },
+    handleNodeClick() {
+      console.log("你好");
+    },
+    handleNodeClick(obj) {},
+    handleCheck(node) {
+      this.checkoutValue = node;
+    },
+    handleRadioChange(obj) {},
+
+    search() {
+      console.log(this.$refs.tree.filter);
+      this.$refs.tree.filter(this.name);
+    },
+    goAddDepart() {
+      uni.navigateTo({
+        url: "./add-depart",
+      });
+    },
+    getTreeDalta(data) {
+      getMyDepartmentTreeList().then(({ code, result }) => {
+        console.log("result==>", result);
+        this.defaultData = result;
+      });
+    },
+    handleNodeExpand(obj) {
+      // console.log('handleNodeExpand', JSON.stringify(obj));
+    },
+    submit() {
+      //点击确定
+      let checkedNodes = this.checkoutValue.node;
+      console.log("checkedNodes", checkedNodes);
+      // checkedNodes:this.checkoutValue.checkedNodes[0]
+      // let pages = getCurrentPages();
+      // var prevPage = pages[pages.length - 2];  //上一个页面
+      // prevPage._data.checkedNodes = this.checkoutValue.checkedNodes[0];
+      let params = {
+        label: checkedNodes.label,
+        value: checkedNodes.data.value,
+        parentId: checkedNodes.parentId,
+      };
+      let pages = getCurrentPages();
+      var prevPage = pages[pages.length - 2]; //上一个页面
+      prevPage._data.selectTree = params; // 将选择是值传到上一个页面
+      // this.$store.commit('setCheckedNodes',params);
+      uni.navigateBack();
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .header{
-            width: 100%;
-            height: 120rpx;
-            background: $my-main-color;
-            .header-cont{
-                .ipt-box{
-                    position: relative;
-                    input{
-                        height: 72rpx;
-                        width: 100%;
-                        height: 100%;
-                        border-radius: 36rpx;
-                        padding:16rpx 16rpx 16rpx 84rpx;
-                        background:#fff;
-                    }
-                    img{
-                        position: absolute;
-                        left:40rpx;
-                        top:50%;
-                        transform: translateY(-50%);
-                        width: 32rpx;
-                        height: 32rpx;
-                    }
-                }
-            }
-            .checkDrop-box{
-                height: 100rpx;
-                width:100%;
-                .checkDrop{
-                // width: 460rpx;
-                position: relative;
-                width:100%;
-                height: 44rpx;
-                background: RGBA(91, 116, 199, 1);
-                border-radius: 22rpx;
-                color: #FFFFFF;
-                .select-title-box{
-                    height: 44rpx;
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                }
-                img{
-                    width: 16rpx;
-                    height: 9rpx;
-                    margin-left:16px
-                }
-            }
-            }
-            
-        }
-        .content {
-            padding-bottom: 100rpx;
-            text-align: center;
-        }
-        .opcity{
-            // opacity: 0.3;
-            background: #b9c4e7 !important;
-            border: none !important;
-        }
-        .btn-group{
-            position: fixed;
-            bottom: 0;
-            z-index: 100;
-            display: flex;
-            height: 100rpx;
-            width: 100%;
-            box-shadow: 1px 1px 8px #c7c7c7;
-            view{
-                width: 100%;
-                height: 100rpx;
-                text-align: center;
-                line-height: 100rpx;
-                font-size:26rpx;
-                    background: $my-main-color;
-                    border: 1px solid $my-main-color;
-                    color: #fff;
-            }
-            }
+.header {
+  width: 100%;
+  height: 120rpx;
+  background: $my-main-color;
+  .header-cont {
+    .ipt-box {
+      position: relative;
+      input {
+        height: 72rpx;
+        width: 100%;
+        height: 100%;
+        border-radius: 36rpx;
+        padding: 16rpx 16rpx 16rpx 84rpx;
+        background: #fff;
+      }
+      img {
+        position: absolute;
+        left: 40rpx;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 32rpx;
+        height: 32rpx;
+      }
+    }
+  }
+  .checkDrop-box {
+    height: 100rpx;
+    width: 100%;
+    .checkDrop {
+      // width: 460rpx;
+      position: relative;
+      width: 100%;
+      height: 44rpx;
+      background: RGBA(91, 116, 199, 1);
+      border-radius: 22rpx;
+      color: #ffffff;
+      .select-title-box {
+        height: 44rpx;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      img {
+        width: 16rpx;
+        height: 9rpx;
+        margin-left: 16px;
+      }
+    }
+  }
+}
+.content {
+  padding-bottom: 100rpx;
+  text-align: center;
+}
+.opcity {
+  // opacity: 0.3;
+  background: #b9c4e7 !important;
+  border: none !important;
+}
+.btn-group {
+  position: fixed;
+  bottom: 0;
+  z-index: 100;
+  display: flex;
+  height: 100rpx;
+  width: 100%;
+  box-shadow: 1px 1px 8px #c7c7c7;
+  view {
+    width: 100%;
+    height: 100rpx;
+    text-align: center;
+    line-height: 100rpx;
+    font-size: 26rpx;
+    background: $my-main-color;
+    border: 1px solid $my-main-color;
+    color: #fff;
+  }
+}
 </style>
